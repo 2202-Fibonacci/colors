ad96bb4fab3646bfcfca8fc03f58b8081078e4bf
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-05-09 14:08:32 -0400

    add markers to subway locations

--- client/components/Map.js
+++ client/components/Map.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { Platform, StyleSheet, Text, View, Dimensions } from "react-native";
 import * as Location from "expo-location";
+import { allStations } from "../../MTA/stations";
 
 export default function Map() {
   const [location, setLocation] = useState(null);
@@ -9,58 +10,68 @@ export default function Map() {
   const [region, setRegion] = useState({
     latitude: 40.752287,
     longitude: -73.993391,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
   });
 
   useEffect(() => {
     (async () => {
+      // get permission to access user's location
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
         return;
       }
 
+      // set current location to users location
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+
+      // set region to center around user's location
+      // setRegion({
+      //   ...region,
+      //   latitude: location.coords.latitude,
+      //   longitude: location.coords.longitude,
+      // });
     })();
   }, []);
 
   let text = "Waiting..";
-  let userLat = "";
-  let userLong = "";
 
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
-    console.log("locaish", location.coords.latitude);
+    console.log("locaish", location.coords.latitude, location.coords.longitude);
     text = "Found";
-    userLat = location.coords.latitude;
-    userLong = location.coords.longitude;
-    // text = JSON.stringify(location);
   }
   console.log("text", text);
-  console.log("location", location);
+
+  const stations = Object.keys(allStations);
 
   return (
     <View style={styles.mapContainer}>
       {text === "Found" ? (
         <Text>
-          Hello you're at {userLat}x{userLong}
+          Hello you're at {location.coords.latitude}x{location.coords.longitude}
         </Text>
       ) : (
         <Text>{text}</Text>
       )}
       <MapView
+        onRegionChange={(region) => setRegion(region)}
         region={region}
-        // initialRegion={{
-        //   latitude: 37.78825,
-        //   longitude: -122.4324,
-        //   latitudeDelta: 0.0922,
-        //   longitudeDelta: 0.0421,
-        // }}
         style={styles.map}
-      />
+      >
+        {stations.map((station) => (
+          <Marker
+            coordinate={{
+              latitude: Number(allStations[station].stop_lat),
+              longitude: Number(allStations[station].stop_lon),
+            }}
+            key={station}
+          />
+        ))}
+      </MapView>
     </View>
   );
 }
