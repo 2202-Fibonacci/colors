6876f4e7abd2e850140e7a4fa9eec92c0719f1ab
Mab Bene <outpt@mac.com>
2022-05-09 11:12:39 -0400

    location start

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  let userLat = '';
+  let userLong = '';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    console.log('locaish', location.coords.latitude);
+    text = 'Found';
+    userLat = location.coords.latitude;
+    userLong = location.coords.longitude;
+    // text = JSON.stringify(location);
+  }
+  console.log('text', text)
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+    {
+      (text === 'Found')
+      ? <Text>Hello you're at {userLat}x{userLong}</Text>
+      : <Text>{text}</Text>
+    }
       <StatusBar style="auto" />
     </View>
   );
@@ -14,10 +49,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    borderWeight: "10px",
-    borderColor: "#00ffff",
     color: "#00ffff",
-    margin: "10px",
+    margin: "10%",
     alignItems: "center",
     justifyContent: "center",
   },
