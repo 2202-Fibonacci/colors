e07c2e2d50596dbf995d3f2826e0f700c704010e
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-05-10 15:49:51 -0400

    navbar placeholder

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Platform, StyleSheet, Text, View } from "react-native";
 import Map from "./client/components/Map";
+import NavBar from "./client/components/NavBar";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <StatusBar style="auto" /> */}
       <Map />
+      <NavBar />
     </View>
   );
 }
