f9f7712aeff4d7c301d4aa0873573d6c8c4761c6
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-05-12 13:55:21 -0400

    add option to select only active service alerts

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,10 +1,13 @@
-
 module.exports = {
-    Query: {
-        info: () => 'This is JML group MTA app',
-        station: (root, {id}, {dataSources}) => dataSources.tripFeed.getStationById(id),
-        arrivalTimes: async (_, {stationId, train, direction}, { dataSources}) => await dataSources.tripFeed.getArrivalTimes(stationId, train, direction),
-        serviceAlert: async (_, {train}, {dataSources}) => await dataSources.alertFeed.getServiceAlert(train),
-        elevatorAlert: async (_, {stationId}, {dataSources}) => await dataSources.elevatorFeed.getElevatorAlerts(stationId)
-    }
-}
+  Query: {
+    info: () => "This is JML group MTA app",
+    station: (root, { id }, { dataSources }) =>
+      dataSources.tripFeed.getStationById(id),
+    arrivalTimes: async (_, { stationId, train, direction }, { dataSources }) =>
+      await dataSources.tripFeed.getArrivalTimes(stationId, train, direction),
+    serviceAlert: async (_, { train, onlyActive }, { dataSources }) =>
+      await dataSources.alertFeed.getServiceAlert(train, onlyActive),
+    elevatorAlert: async (_, { stationId }, { dataSources }) =>
+      await dataSources.elevatorFeed.getElevatorAlerts(stationId),
+  },
+};
