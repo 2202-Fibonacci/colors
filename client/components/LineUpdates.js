46bf3856471dc4e5e1420aa705bd710b4f518344
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-05-13 11:58:05 -0400

    station update query in LineUpdates component

--- client/components/LineUpdates.js
+++ client/components/LineUpdates.js
@@ -21,19 +21,37 @@ const NEXT_ARRIVALS = gql`
   }
 `;
 
+const STATION_UPDATE = gql`
+  query StationQuery($stationId: String!) {
+    stationUpdate(stationId: $stationId) {
+      routeId
+      nextArrivals {
+        tripId
+        direction
+        arrivalTime
+      }
+    }
+  }
+`;
+
 const maxUpdates = 3;
 
 export default function LineUpdates({ station, line }) {
   // hook to get arrival data
-  const { data, loading, error, refetch } = useQuery(NEXT_ARRIVALS, {
-    variables: { stationId: station, train: line } /*, direction: "N" */,
-    pollInterval: 1000,
+  // const { data, loading, error, refetch } = useQuery(NEXT_ARRIVALS, {
+  //   variables: { stationId: station, train: line } /*, direction: "N" */,
+  //   pollInterval: 1000,
+  // });
+  const { data, loading, error, refetch } = useQuery(STATION_UPDATE, {
+    variables: { stationId: station } /*, direction: "N" */,
   });
 
   // prevent refetch on first render by comparing with previous props
   useEffect(() => {
     refetch();
   }, [line]);
+
+  const stationUpdate = (data && data.stationUpdate) || [];
 
   return (
     <View style={styles.updatesContainer}>
@@ -42,27 +60,9 @@ export default function LineUpdates({ station, line }) {
       ) : error ? (
         <Text>Error: {error.message}</Text>
       ) : (
-        data.arrivalTimes.nextArrivals.map((arrival, i) =>
-          i < maxUpdates ? (
-            <Text style={styles.arrival} key={arrival.tripId}>
-              {" "}
-              {data.arrivalTimes.routeId} {"  "}
-              {arrival.direction === "N"
-                ? allStations[station].north_label
-                : allStations[station].south_label}
-              {"                             ".slice(
-                0,
-                30 -
-                  (arrival.direction === "N"
-                    ? allStations[station].north_label.length
-                    : allStations[station].south_label.length)
-              )}
-              {"  ".slice(0, arrival.arrivalTime > 9 ? 1 : 2)}
-              {arrival.arrivalTime}
-              {"M "}
-            </Text>
-          ) : null
-        )
+        stationUpdate.map((update) => (
+          <Text style={styles.arrival}>{update.routeId}</Text>
+        ))
       )}
     </View>
   );
@@ -90,3 +90,25 @@ const styles = StyleSheet.create({
     paddingVertical: "1%",
   },
 });
+
+// data.arrivalTimes.nextArrivals.map((arrival, i) =>
+// i < maxUpdates ? (
+//   <Text style={styles.arrival} key={arrival.tripId}>
+//     {" "}
+//     {data.arrivalTimes.routeId} {"  "}
+//     {arrival.direction === "N"
+//       ? allStations[station].north_label
+//       : allStations[station].south_label}
+//     {"                             ".slice(
+//       0,
+//       30 -
+//         (arrival.direction === "N"
+//           ? allStations[station].north_label.length
+//           : allStations[station].south_label.length)
+//     )}
+//     {"  ".slice(0, arrival.arrivalTime > 9 ? 1 : 2)}
+//     {arrival.arrivalTime}
+//     {"M "}
+//   </Text>
+// ) : null
+// )
