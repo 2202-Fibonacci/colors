e56c4fc49516eaade63536fdaa5941167db42922
Mab Bene <outpt@mac.com>
2022-05-16 11:44:39 -0400

    front end refinement

--- client/components/StationAlerts.js
+++ client/components/StationAlerts.js
@@ -32,36 +32,77 @@ export default function StationAlerts( {station}) {
 
   if(loading) return <Text>Loading . . .</Text>
   if(error) console.log('Error ', error)
+  
+  const {elevator, escalator} = (data) ? groupAlerts(data.elevatorAlert) : [];
 
   return (
     <View style={styles.alertsContainer}>
-
-      {loading ? (<Text>Loading ...</Text>)
-        : error? (<Text>Error: {error.message}</Text>)
-        :
-        (data.elevatorAlert
-          .map((alert) => (
-        <View key={alert.equipment}>
-          <Text style={styles.alert}>
-            {alert.equipmenttype === "ES" ? "Escalator" : "Elevator"} outage:
-          </Text>
-          <Text>{alert.serving}</Text>
-        </View>
-      )))}
+      {
+        (elevator.length>0)
+        ? <Text style={styles.alert}>Elevator Alerts</Text>
+        : null
+      }
+      {
+        (elevator.length>0)
+        ? elevator.map((alert, i) => (
+            <View style={styles.alertLi} key={`esc_${station}-${i}`}>
+              <Text style={styles.alert}>{"• "}</Text>
+              <Text style={styles.alertContent}>{alert}</Text>
+            </View>
+        ))
+        : null
+      }
+      {
+        (escalator.length>0)
+        ? <Text style={styles.alert}>Escalator Alerts</Text>
+        : null
+      }
+      {
+        (escalator.length>0)
+        ? escalator.map((alert, i) => (
+            <View style={styles.alertLi} key={`esc_${station}-${i}`}>
+              <Text style={styles.alert}>{"• "}</Text>
+              <Text style={styles.alertContent}>{alert}</Text>
+            </View>
+          ))
+        : null
+      }
     </View>
   );
 }
 
+const groupAlerts = (data) => {
+  let elevator = [];
+  let escalator = [];
+  for (let i=0; i<data.length; i++) {
+    console.log('i:', data[i]);
+    data[i].equipmenttype === 'ES'
+    ? escalator.push(data[i].serving)
+    : elevator.push(data[i].serving)
+  }
+  console.log('elev:', elevator.length, 'esc:', escalator.length)
+  return {elevator, escalator};
+}
+
 const styles = StyleSheet.create({
   alertsContainer: {
-    backgroundColor: "#fff",
+    backgroundColor: "#000",
     padding: "2%",
-    color: "#00ffff",
+    color: "#eeff00",
     alignItems: "flex-start",
     justifyContent: "flex-start",
     width: "100%",
   },
+  alertLi: {
+    display: "flex",
+    flexDirection: "row",
+  },
   alert: {
     fontWeight: "bold",
+    color: "#eeff00",
+  },
+  alertContent: {
+    fontWeight: "normal",
+    color: "#eeff00",
   },
 });
