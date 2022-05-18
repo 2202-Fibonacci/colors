6e26f58b24f46899b11569a229a55e8f30c2ba54
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-05-18 11:51:32 -0400

    remove console logs

--- App.js
+++ App.js
@@ -15,11 +15,6 @@ import {
   gql,
 } from "@apollo/client";
 
-console.log("EXPO_ENV", process.env.EXPO_ENV);
-// console.log("NODE_ENV", process.env);
-// console.log("PORT", process.env.PORT);
-console.log("API_URL", Constants.manifest.extra.API_URL);
-
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: Constants.manifest.extra.API_URL,
