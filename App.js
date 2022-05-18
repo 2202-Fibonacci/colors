1d6fc53228a3c3b6f7d74af44184a61b465b5a74
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-05-17 21:49:19 -0400

    access node env with brackets

--- App.js
+++ App.js
@@ -15,8 +15,8 @@ import {
   gql,
 } from "@apollo/client";
 
-console.log("NODE_ENV", process.env.NODE_ENV);
-console.log("PORT", process.env.PORT);
+console.log("NODE_ENV", process.env["NODE_ENV"]);
+// console.log("PORT", process.env.PORT);
 console.log("API_URL", Constants.manifest.extra.API_URL);
 
 const client = new ApolloClient({
--- app.config.js
+++ app.config.js
@@ -1,10 +1,9 @@
 export default function ({ config }) {
-  // config paramter
   return {
     ...config,
     extra: {
       API_URL:
-        process.env.NODE_ENV === "PRODUCTION"
+        process.env["NODE_ENV"] === "production"
           ? "https://findmyline.herokuapp.com"
           : "http://localhost:4000",
     },
