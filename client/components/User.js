830ed03ce9e8a216da2a502c05ba6b71853f3afe
laurynn <laurynnlowe@users.noreply.github.com>
2022-05-19 15:45:27 -0400

    experiments with auth token

--- client/components/User.js
+++ client/components/User.js
@@ -1,11 +1,29 @@
 import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Login from './Login';
+import Profile from './Profile';
+import Logout from './Logout'
+// import {getUserId, getTokenPayload} from '../../server/utils'
+import * as SecureStore from 'expo-secure-store';
+
 
 export default function UserScreen() {
+
+  const authToken = async () =>{
+    let result = await SecureStore.getItemAsync("key")
+    if (result){
+    console.log(result) 
+    return result}
+    else return false
+  }
+  authToken()
+
+
   return (
     <View style={styles.container}>
-    <Login />
+
+      {authToken ? <Profile /> : <Login />}
+      <Logout />
       {/* <Text style={styles.txt}>User Page (switch contents on logged in status)</Text> */}
     </View>
   )
